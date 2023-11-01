6b1895b37acd4cddab65bfa4c61d3af6668cf7ee
bisen <210103214@stu.sdu.edu.kz>
2023-11-01 21:50:51 +0600

    first

---
+++ app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.recyclerviewkotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewkotlin.ui.theme.RecyclerViewKotlinTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var dataList: ArrayList<DataClass>
+    lateinit var imageList: Array<Int>
+    lateinit var titleList: Array<String>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageList = arrayOf(
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g,
+            R.drawable.h
+        )
+
+        titleList = arrayOf(
+            "Tiger",
+            "Lion",
+            "Rabbit",
+            "Monkey",
+            "Elephant",
+            "Crocodile",
+            "Wolf",
+            "Shark"
+        )
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        dataList = arrayListOf<DataClass>()
+        getData()
+    }
+
+    private fun getData() {
+        for (i in imageList.indices) {
+            val dataClass = DataClass(imageList[i], titleList[i])
+            dataList.add(dataClass)
+        }
+        recyclerView.adapter = AdapterClass(dataList)
+    }
+}
+
+
